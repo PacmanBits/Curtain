ab965faa844f80e441a3a2a2a84d82da7be20d1f
Sandy <sandy@blue-geek.net>
2015-07-20 12:45:45 -0400

    Added support for iFrames
    - Instances in iFrames will now activate curtain at top level

--- Curtain.js
+++ Curtain.js
@@ -9,121 +9,134 @@ var Curtain = {
 
 (function($)
 {
-	var curtainEl = $(document.createElement("div"))
-		.css({
+	// if the current page has been included as an iFrame we'll use the Curtain that's defined at the top so that it appears over everything
+	if(top.document.CurtainDocTopAlias)
+	{
+		// Point the Curtain functions at this level to the top Curtain
+		for(var f in top.document.CurtainDocTopAlias)
+			Curtain[f] = top.document.CurtainDocTopAlias[f];
+	}
+	else
+	{
+		// Alias to place accessible at all levels
+		top.document.CurtainDocTopAlias = Curtain;
+		
+		var curtainEl = $(document.createElement("div"))
+			.css({
+				position:        "fixed",
+				top:             "0",
+				left:            "0",
+				right:           "0",
+				bottom:          "0",
+				backgroundColor: "black",
+				opacity:         "0.5",
+				display:         "none",
+				zIndex:          998
+			})
+			.click(function(){ Curtain.Hide(); })
+		;
+		
+		var frame   = $(document.createElement("div")).css({
 			position:        "fixed",
-			top:             "0",
-			left:            "0",
-			right:           "0",
-			bottom:          "0",
-			backgroundColor: "black",
-			opacity:         "0.5",
+			top:             "50%",
+			left:            "50%",
+			transform:       "translate(-50%,-50%)",
 			display:         "none",
-			zIndex:          998
-		})
-		.click(function(){ Curtain.Hide(); })
-	;
-	
-	var frame   = $(document.createElement("div")).css({
-		position:        "fixed",
-		top:             "50%",
-		left:            "50%",
-		transform:       "translate(-50%,-50%)",
-		display:         "none",
-		zIndex:          999
-	});
-	
-	var iFrame  = $(document.createElement("iframe"))
-		.css({
-			width:           "100%",
-			height:          "100%",
-			display:         "none",
-			backgroundColor: "white",
-			border:          "none"
-		})
-		.appendTo(frame)
-	;
-	
-	var contentEl = $(document.createElement("div"))
-		.css({
-			width:           "100%",
-			height:          "100%",
-			display:         "none"
-		})
-		.appendTo(frame)
-	;
-	
-	var defaultOptions = {
-		width   : "auto" ,
-		height  : "auto" ,
-		URL     : null   ,
-		content : null
-	}
-	
-	Curtain.Show = function(options)
-	{
-		var opts = $.extend({}, defaultOptions, options);
+			zIndex:          999
+		});
 		
-		if(opts.URL)
-			LoadURL(opts.URL);
-		else if(opts.content)
-			SetContent(opts.content);
+		var iFrame  = $(document.createElement("iframe"))
+			.css({
+				width:           "100%",
+				height:          "100%",
+				display:         "none",
+				backgroundColor: "white",
+				border:          "none"
+			})
+			.appendTo(frame)
+		;
 		
-		SetSize(opts.width, opts.height);
+		var contentEl = $(document.createElement("div"))
+			.css({
+				width:           "100%",
+				height:          "100%",
+				display:         "none"
+			})
+			.appendTo(frame)
+		;
 		
-		curtainEl.show();
-		frame.show();
-	}
-	
-	Curtain.Hide = function()
-	{
-		Clear();
+		var defaultOptions = {
+			width   : "auto" ,
+			height  : "auto" ,
+			URL     : null   ,
+			content : null
+		}
 		
-		curtainEl.hide();
-		frame.hide();
-	}
-	
-	function LoadURL(URL)
-	{
-		iFrame.hide();
-		iFrame.attr("src", URL);
+		Curtain.Show = function(options)
+		{
+			var opts = $.extend({}, defaultOptions, options);
+			
+			if(opts.URL)
+				LoadURL(opts.URL);
+			else if(opts.content)
+				SetContent(opts.content);
+			
+			SetSize(opts.width, opts.height);
+			
+			curtainEl.show();
+			frame.show();
+		}
 		
-		contentEl.hide();
-		iFrame.show();
-//		iFrame.load(function()
-//		{
-//			$(this).fadeIn(10000);
-//		});
-	}
-	
-	function SetContent(content)
-	{
-		contentEl.append($(content));
+		Curtain.Hide = function()
+		{
+			Clear();
+			
+			curtainEl.hide();
+			frame.hide();
+		}
 		
-		contentEl.show();
-		iFrame.hide();
-	}
-	
-	function SetSize(width, height)
-	{
-		frame.css({
-			width  : width,
-			height : height
+		function LoadURL(URL)
+		{
+			iFrame.hide();
+			iFrame.attr("src", URL);
+			
+			contentEl.hide();
+			iFrame.show();
+	//		iFrame.load(function()
+	//		{
+	//			$(this).fadeIn(10000);
+	//		});
+		}
+		
+		function SetContent(content)
+		{
+			contentEl.append($(content));
+			
+			contentEl.show();
+			iFrame.hide();
+		}
+		
+		function SetSize(width, height)
+		{
+			frame.css({
+				width  : width,
+				height : height
+			});
+		}
+		
+		function Clear()
+		{
+			iFrame.attr("src", "");
+			contentEl.empty();
+			
+			contentEl.hide();
+			iFrame.hide();
+		}
+		
+		$(function()
+		{
+			curtainEl.appendTo($("body"));
+			frame.appendTo($("body"));
 		});
 	}
-	
-	function Clear()
-	{
-		iFrame.attr("src", "");
-		contentEl.empty();
-		
-		contentEl.hide();
-		iFrame.hide();
-	}
-	
-	$(function()
-	{
-		curtainEl.appendTo($("body"));
-		frame.appendTo($("body"));
-	});
 })(jQuery)
